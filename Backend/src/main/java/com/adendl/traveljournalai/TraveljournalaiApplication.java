e510f542239613667b5545e451cd10c23941f20c
ADEN DE LEEUW <adendeleeuw@outlook.com>
2025-06-29 20:14:24 +1000

    initialising project

---
+++ Backend/src/main/java/com/adendl/traveljournalai/TraveljournalaiApplication.java
@@ -0,0 +1,13 @@
+package com.adendl.traveljournalai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraveljournalaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraveljournalaiApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/adendl/traveljournalai/TraveljournalaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adendl.traveljournalai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraveljournalaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
