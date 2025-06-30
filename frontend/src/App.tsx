0802a94112051a5845ee42954b4d413182f9417f
Aden de Leeuw <90319180+adendl@users.noreply.github.com>
2025-06-30 12:42:42 +1000

    Updated the header logo to redirect to home

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
+import SignUp from './pages/SignUp'; // Add SignUp import
 import './styles/index.css';
 
 const App: React.FC = () => {
@@ -11,7 +12,7 @@ const App: React.FC = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<div>Sign Up Page (TBD)</div>} />
+          <Route path="/signup" element={<SignUp />} /> {/* Add SignUp route */}
         </Routes>
       </div>
     </Router>
