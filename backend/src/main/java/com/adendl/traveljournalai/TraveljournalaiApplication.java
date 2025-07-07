bc5f2e74e5dc0803252f6d932992e97e2112d51f
Aden de Leeuw <90319180+adendl@users.noreply.github.com>
2025-07-07 20:31:34 +1000

    increased logging level and added app startup log message

--- backend/src/main/java/com/adendl/traveljournalai/TraveljournalaiApplication.java
+++ backend/src/main/java/com/adendl/traveljournalai/TraveljournalaiApplication.java
@@ -1,6 +1,8 @@
 package com.adendl.traveljournalai;
 
 import org.locationtech.jts.geom.GeometryFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -9,8 +11,12 @@ import org.springframework.context.annotation.Configuration;
 @SpringBootApplication
 public class TraveljournalaiApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(TraveljournalaiApplication.class);
+
 	public static void main(String[] args) {
+		logger.info("Starting TraveljournalaiApplication...");
 		SpringApplication.run(TraveljournalaiApplication.class, args);
+		logger.info("TraveljournalaiApplication started successfully.");
 	}
 
 }
